50a9ebc851acb6a0978e1b33d816e848b1ca7549
LJulianMorales <2019452092@teschi.edu.mx>
2023-11-09 18:57:49 -0600

    V2

--- apiDjango/settings.py
+++ apiDjango/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = 'django-insecure-jto3ms%p-rqyr@afwkg=9m39-mqp$)^n6&xo^icq85lil)x&f&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -158,7 +157,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# settings.py
+
+STATIC_URL = '/api/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
